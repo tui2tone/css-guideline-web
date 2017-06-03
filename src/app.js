d37acb711f47f1995d684ef082b12825a0a0f426
Charuwit Nodthaisong <coladevteam@gmail.com>
2017-06-04 04:17:49 +0700

    initialize with react

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+require('./app.scss')
+
+ReactDOM.render(
+  <div>
+  	test
+  </div>
+  , document.getElementById('app'));
